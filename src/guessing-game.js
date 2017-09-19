b869b00deb5a1830640e09268e781fe64735d79f
dbukatyy <dbukatyy@mail.ru>
2017-09-19 18:05:34 +0300

    create class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+      this.number = null
     }
 
-    guess() {
+    setRange(min, max) {
+      this.number = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+     
+    }
 
+    guess(userNumber) {
+      if (userNumber > this.number) {
+        this.greater();
+      } else if (userNumber < this.number) {
+        this.lower();
+      } else if (userNumber === this.number) {
+        console.log('Вы угадали')
+      }
     }
 
     lower() {
-
+      console.log('ваше число меньше')
     }
 
     greater() {
-
+      console.log('ваше число больше')
     }
 }
 
